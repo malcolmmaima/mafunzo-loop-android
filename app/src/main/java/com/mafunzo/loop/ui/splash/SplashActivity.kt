2e9cdf21284a0fc868405c6f1c6ba758e0276057
malcolm maima <malcolmmaima@gmail.com>
2022-06-22 18:11:56 +0300

    setup splash activity

--- app/src/main/java/com/mafunzo/loop/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/mafunzo/loop/ui/splash/SplashActivity.kt
@@ -2,11 +2,31 @@ package com.mafunzo.loop.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import com.mafunzo.loop.R
+import com.mafunzo.loop.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val viewModel = SplashViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        initSplash()
+    }
+
+    private fun initSplash() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun loadAuth(){
+
+    }
+
+    private fun loadMainActivity(){
+
     }
 }
