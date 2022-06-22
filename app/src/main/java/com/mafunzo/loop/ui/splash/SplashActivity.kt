e1939fb73c5921ed59b4d0201f7be3a10fe7aa23
malcolm maima <malcolmmaima@gmail.com>
2022-06-22 17:15:35 +0300

    setup splash screen

--- app/src/main/java/com/mafunzo/loop/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/mafunzo/loop/ui/splash/SplashActivity.kt
@@ -1,12 +1,12 @@
-package com.mafunzo.loop.ui
+package com.mafunzo.loop.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mafunzo.loop.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
