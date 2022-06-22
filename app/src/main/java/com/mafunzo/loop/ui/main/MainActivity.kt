2190adada3dfde877ef6b43068b4af5565c4cafe
malcolm maima <malcolmmaima@gmail.com>
2022-06-22 18:12:43 +0300

    minor

--- app/src/main/java/com/mafunzo/loop/ui/main/MainActivity.kt
+++ app/src/main/java/com/mafunzo/loop/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mafunzo.loop.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mafunzo.loop.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
