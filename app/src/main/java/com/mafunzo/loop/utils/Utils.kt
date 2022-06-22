8dbb17c754ae4eed8147d4fda89ef0952fd32c2d
malcolm maima <malcolmmaima@gmail.com>
2022-06-22 17:42:12 +0300

    add utils

---
+++ app/src/main/java/com/mafunzo/loop/utils/Utils.kt
@@ -0,0 +1,54 @@
+package com.mafunzo.loop.utils
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+
+
+
+fun View.snackbar(message: String, action: (() -> Unit)? = null) {
+    val snackbar = Snackbar.make(this, message, Snackbar.LENGTH_LONG)
+    action?.let {
+        snackbar.setAction("Retry") {
+            it()
+        }
+    }
+    snackbar.show()
+}
+
+private const val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+
+fun validateEmail(email: String): Boolean {
+    return email.matches(emailPattern.toRegex())
+}
+
+fun formatDate(date: String, originalFormat: String, expectedFormat: String): String {
+    return try {
+        val originalDateTime = SimpleDateFormat(originalFormat).parse(date)
+        SimpleDateFormat(expectedFormat).format(originalDateTime!!)
+    } catch (e: Exception) {
+        date
+    }
+}
+
+fun View.hideKeyboard() {
+    val closeKeyboard =
+        this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    closeKeyboard.hideSoftInputFromWindow(this.windowToken, 0)
+}
+
+fun EditText.showKeyboard() {
+    if (requestFocus()) {
+        (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+            .showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+        setSelection(text.length)
+    }
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
