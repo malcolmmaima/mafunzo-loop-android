56a5e15a502828113dba5cd33ba49f920b8e1e3d
malcolm maima <malcolmmaima@gmail.com>
2022-07-19 15:49:22 +0300

    event recyclerview + event items

--- app/src/main/java/com/mafunzo/loop/ui/calendar/fragments/CalendarFragment.kt
+++ app/src/main/java/com/mafunzo/loop/ui/calendar/fragments/CalendarFragment.kt
@@ -1,17 +1,20 @@
 package com.mafunzo.loop.ui.calendar.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.mafunzo.loop.databinding.FragmentCalendarBinding
 import com.mafunzo.loop.ui.calendar.viewmodel.CalendarViewModel
 import com.mafunzo.loop.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class CalendarFragment : Fragment() {
@@ -29,6 +32,21 @@ class CalendarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpToolbar()
+        initializeCalendar()
+    }
+
+    private fun initializeCalendar() {
+
+        binding.calendarView.setOnDateChangeListener {
+                _, calYear, calMonth, calDay ->
+            val year = calYear.toString()
+            val month = (calMonth+1).toString()
+            val day = calDay.toString()
+            val date = "$year-$month-$day"
+
+            Toast.makeText(context, date, Toast.LENGTH_LONG).show()
+        }
+
     }
 
     private fun setUpToolbar() {
