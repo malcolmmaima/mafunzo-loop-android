ba519d33d13be8f47df22c9bd285de066110d54e
malcolm maima <malcolmmaima@gmail.com>
2022-07-20 21:52:17 +0300

    unnecessary onCreate

--- app/src/main/java/com/mafunzo/loop/ui/requests/fragments/SubmitRequestFragment.kt
+++ app/src/main/java/com/mafunzo/loop/ui/requests/fragments/SubmitRequestFragment.kt
@@ -28,10 +28,6 @@ class SubmitRequestFragment : Fragment() {
     private lateinit var binding: FragmentSubmitRequestBinding
     private val submitRequestViewModel: SubmitRequestViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
